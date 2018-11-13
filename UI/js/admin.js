1010b4e8daeb0a4c1edad2d0815471f3f680c7a5
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-11-13 19:56:31 +0100

    chore: Login and Signup form UI improvement [delivers #161933414]

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -1,11 +1,18 @@
 const login = document.querySelectorAll('.login');
 const signUpAdm = document.querySelectorAll('.signup');
+const logout = document.querySelectorAll('.logout');
 const orderBtn = document.querySelectorAll('.order-btn');
 
 const adminInit = () => {
 	signUpAdm.forEach(das => (das.style.display = 'none'));
 	orderBtn.forEach(das => (das.style.display = 'none'));
 	login.forEach(das => (das.style.display = 'none'));
+
+	logout.forEach(item =>
+		item.addEventListener('click', () => {
+			window.location.href = 'index.html';
+		})
+	);
 };
 
 adminInit();
--- UI/js/user.js
+++ UI/js/user.js
@@ -2,6 +2,7 @@ const accordion = document.querySelectorAll('.accordion');
 const orderBtn = document.querySelectorAll('.order-btn');
 const signUpBtn = document.querySelectorAll('.signup');
 const login = document.querySelectorAll('.login');
+const logout = document.querySelectorAll('.logout');
 
 for (let i = 0; i < accordion.length; i++) {
 	accordion[i].addEventListener('click', function() {
@@ -21,6 +22,12 @@ const userInit = () => {
 	orderBtn.forEach(order =>
 		order.addEventListener('click', () => {
 			console.log('Order button');
+		})
+	);
+
+	logout.forEach(item =>
+		item.addEventListener('click', () => {
+			window.location.href = 'index.html';
 		})
 	);
 };
