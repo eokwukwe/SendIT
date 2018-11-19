38dc2b31de34eaa3eda632bceeb05a6e2122853e
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-11-19 20:30:06 +0100

    feature: user can veiw all his orders [delivers #162014053]

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
-import ParcelOrders from '../controllers/order.controller';
+import Orders from '../controllers/order.controller';
 import { validateOrder } from '../middleware/orderValidation';
+import Auth from '../middleware/auth';
 
 const router = Router();
 
-router.get('/parcels', ParcelOrders.getAllOrders);
-router.get('/parcels/:parcelId', ParcelOrders.getOneOrder);
-router.get('/users/:userId/parcels', ParcelOrders.getOrdersbyUser);
-router.post('/parcels', validateOrder.validOrder, ParcelOrders.createOrder);
-router.put('/parcels/:parcelId/cancel', ParcelOrders.cancelOrder);
+router.get('/parcels', Orders.getAllOrders);
+router.get('/parcels/:parcelId', Orders.getOneOrder);
+router.get(
+	'/users/:userId/parcels',
+	Auth.verifyUserToken,
+	Orders.getOrdersbyUser
+);
+router.post('/parcels', validateOrder.validOrder, Orders.createOrder);
+router.put('/parcels/:parcelId/cancel', Orders.cancelOrder);
 
 export default router;
