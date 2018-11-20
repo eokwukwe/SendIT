619bcb8926b7c5f37a7b16fa2fe13217b10e13b9
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-11-20 04:36:11 +0100

    feature: admin can change order status [delivers #162014477]

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -5,14 +5,29 @@ import Auth from '../middleware/auth';
 
 const router = Router();
 
-router.get('/parcels', Orders.getAllOrders);
-router.get('/parcels/:parcelId', Orders.getOneOrder);
+router.get('/parcels', Auth.verifyAdminToken, Orders.getAllOrders);
+router.get('/parcels/:parcelId', Auth.verifyUserToken, Orders.getOneOrder);
 router.get(
 	'/users/:userId/parcels',
 	Auth.verifyUserToken,
 	Orders.getOrdersbyUser
 );
-router.post('/parcels', validateOrder.validOrder, Orders.createOrder);
-router.put('/parcels/:parcelId/cancel', Orders.cancelOrder);
+router.post(
+	'/parcels',
+	Auth.verifyUserToken,
+	validateOrder.validOrder,
+	Orders.createOrder
+);
+router.put(
+	'/parcels/:parcelId/cancel',
+	Auth.verifyUserToken,
+	Orders.cancelOrder
+);
+router.put('/parcels/:parcelId/destination', Orders.changeOrderDestination);
+router.put(
+	'/parcels/:parcelId/status',
+	Auth.verifyAdminToken,
+	Orders.changeOrderStatus
+);
 
 export default router;
