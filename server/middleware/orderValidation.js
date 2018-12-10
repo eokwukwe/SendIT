582a68dd24a2018f01900fa05ca387e868e1b177
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2018-12-10 17:12:51 +0100

    restructure users and orders table [delivers #162549327]

--- server/middleware/orderValidation.js
+++ server/middleware/orderValidation.js
@@ -2,115 +2,52 @@ import { check } from 'express-validator/check';
 
 const validateOrder = {
   validOrder: [
-    check('parcelDescription')
-      .isLength({ min: 10 })
+    check('description')
+      .isLength({ min: 3 })
       .withMessage('parcel description is too short')
       .isString()
       .trim(),
-
-    check('parcelWeight')
+    check('weight')
       .isDecimal()
       .withMessage('weight must be decimal value')
       .trim(),
-
-    check('fromAddress')
+    check('destination')
       .isLength({ min: 5 })
       .withMessage('address is too short')
-      .matches(/^[a-zA-Z-\d,\s]+$/)
+      .matches(/^[a-zA-Z-\d,-\s]+$/)
       .withMessage('invalid input, please enter a valid address')
       .trim(),
-
-    check('fromCity')
-      .isLength({ min: 3 })
-      .withMessage('city name is too short')
-      .isAlpha()
-      .withMessage('city name cannot be number')
-      .trim(),
-
-    check('fromCountry')
-      .isLength({ min: 3 })
-      .withMessage('country name is too short')
-      .isAlpha()
-      .withMessage('country name cannot be number')
-      .trim(),
-
-    check('toAddress')
+    check('pickup')
       .isLength({ min: 5 })
       .withMessage('destination address is too short')
-      .matches(/^[a-zA-Z-\d,\s]+$/)
+      .matches(/^[a-zA-Z-\d,-\s]+$/)
       .withMessage('invalid input, please enter a valid address')
       .trim(),
-
-    check('toCity')
-      .isLength({ min: 3 })
-      .withMessage('destination city name is too short')
-      .isAlpha()
-      .withMessage('destination city name cannot be number')
-      .trim(),
-
-    check('toCountry')
-      .isLength({ min: 3 })
-      .withMessage('destination country name is too short')
-      .isAlpha()
-      .withMessage('destination country name cannot be number')
-      .trim(),
-
     check('receiver')
       .isLength({ min: 2 })
       .withMessage('receiver name too short')
-      .isAlpha()
-      .withMessage('receiver name cannot be number')
       .trim(),
-
-    check('receiverPhone')
+    check('phone')
       .matches(/^[0][7-9]{1}[0-1]{1}[\s]?[0-9][-\s.]?[0-9]{3}[-\s.]?[0-9]{4}$/)
       .withMessage('invalid receiver phone number')
       .trim()
   ],
 
   validDestination: [
-    check('toAddress')
+    check('destination')
       .isLength({ min: 5 })
-      .withMessage('destination address is too short')
-      .matches(/^[a-zA-Z-\d,\s]+$/)
+      .withMessage('address is too short')
+      .matches(/^[a-zA-Z-\d,-\s]+$/)
       .withMessage('invalid input, please enter a valid address')
-      .trim(),
-
-    check('toCity')
-      .isLength({ min: 3 })
-      .withMessage('destination city name is too short')
-      .isAlpha()
-      .withMessage('destination city name cannot be number')
-      .trim(),
-
-    check('toCountry')
-      .isLength({ min: 3 })
-      .withMessage('destination country name is too short')
-      .isAlpha()
-      .withMessage('destination country name cannot be number')
       .trim()
   ],
 
   validLocation: [
-    check('fromAddress')
+    check('location')
       .isLength({ min: 5 })
       .withMessage('address is too short')
-      .matches(/^[a-zA-Z-\d,\s]+$/)
+      .matches(/^[a-zA-Z-\d,-\s]+$/)
       .withMessage('invalid input, please enter a valid address')
-      .trim(),
-
-    check('fromCity')
-      .isLength({ min: 3 })
-      .withMessage('city name is too short')
-      .isAlpha()
-      .withMessage('city name cannot be number')
-      .trim(),
-
-    check('fromCountry')
-      .isLength({ min: 3 })
-      .withMessage('country name is too short')
-      .isAlpha()
-      .withMessage('country name cannot be number')
       .trim()
   ]
 };
